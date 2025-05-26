3ad70b8b8cd01cc8255d7d9a309cd3aa2f31d044
Ítalo Santana <italoeugenio539@gmail.com>
2025-05-26 16:18:02 -0300

    init project

---
+++ src/main/java/com/italo/TaskBrother/TaskBrotherApplication.java
@@ -0,0 +1,13 @@
+package com.italo.TaskBrother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBrotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBrotherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/italo/TaskBrother/TaskBrotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italo.TaskBrother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBrotherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
