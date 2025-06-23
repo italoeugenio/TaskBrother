b66657a7b5399db6b5f29c1b618da5c7d747a623
Ítalo Santana <italoeugenio539@gmail.com>
2025-06-23 15:49:14 -0300

    adding family service and controler(save)

---
+++ src/main/java/com/italo/TaskBrother/controller/Familycontroller.java
@@ -0,0 +1,23 @@
+package com.italo.TaskBrother.controller;
+
+import com.italo.TaskBrother.models.dtos.FamilyRecordDTO;
+import com.italo.TaskBrother.models.entities.FamilyModel;
+import com.italo.TaskBrother.services.FamilyService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("family")
+public class Familycontroller {
+
+    @Autowired
+    private FamilyService familyService;
+
+    @PostMapping("/post")
+    public ResponseEntity<FamilyModel> saveFamily( @RequestBody @Valid FamilyRecordDTO familyRecordDTO) {
+        return familyService.saveFamily(familyRecordDTO);
+    }
+}
+
---
+++ src/main/java/com/italo/TaskBrother/services/FamilyService.java
@@ -0,0 +1,27 @@
+package com.italo.TaskBrother.services;
+
+import com.italo.TaskBrother.models.dtos.FamilyRecordDTO;
+import com.italo.TaskBrother.models.entities.FamilyModel;
+import com.italo.TaskBrother.models.repository.FamilyRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FamilyService {
+
+    @Autowired
+    private FamilyRepository familyRepository;
+
+    public ResponseEntity<FamilyModel> saveFamily(@RequestBody FamilyRecordDTO familyRecordDTO){
+        var familyMdodel = new FamilyModel();
+        BeanUtils.copyProperties(familyRecordDTO, familyMdodel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(familyRepository.save(familyMdodel));
+    }
+}
