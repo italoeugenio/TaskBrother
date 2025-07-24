61924ead9562f8af0e5b1c6582198707b0f60c15
Ítalo Santana <italoeugenio539@gmail.com>
2025-07-24 16:54:14 -0300

    adding changePassword

--- src/main/java/com/italo/TaskBrother/controller/UserController.java
+++ src/main/java/com/italo/TaskBrother/controller/UserController.java
@@ -9,7 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("user")
@@ -18,17 +20,23 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/post")
-    public ResponseEntity<Object> saveUser(@RequestBody @Valid UserRecordDTO userRecordDTO){
+    public ResponseEntity<Object> saveUser(@RequestBody @Valid UserRecordDTO userRecordDTO) {
         return userService.saveUser(userRecordDTO);
     }
 
     @GetMapping("/get/all")
-    public ResponseEntity<List<UserModel>> getAllUsers(){
+    public ResponseEntity<List<UserModel>> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/get/{email}")
-    public ResponseEntity<Optional<UserModel>> getByEmail(@PathVariable("email") @Valid String email){
-        return  userService.getByEmail(email);
+    public ResponseEntity<Optional<UserModel>> getByEmail(@PathVariable("email") @Valid String email) {
+        return userService.getByEmail(email);
+    }
+
+    @PatchMapping("/change-password/{id}")
+    public ResponseEntity<UserModel> changePassword(@PathVariable("id") UUID id, @RequestBody Map<String, String> payload) {
+        String newPassword = payload.get("password");
+        return userService.changePassword(id,newPassword);
     }
 }
--- src/main/java/com/italo/TaskBrother/services/UserService.java
+++ src/main/java/com/italo/TaskBrother/services/UserService.java
@@ -20,12 +20,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public ResponseEntity<Object> saveUser(@RequestBody UserRecordDTO userRecordDTO){
-        Optional<UserModel> optionalUserModel = userRepository.findByemail(userRecordDTO.email());
-        if(!EmailValidator.checkIfEmailIsValid(userRecordDTO.email())){
+    public ResponseEntity<Object> saveUser(@RequestBody UserRecordDTO userRecordDTO) {
+        Optional<UserModel> optionalUserModel = userRepository.findByEmail(userRecordDTO.email());
+        if (!EmailValidator.checkIfEmailIsValid(userRecordDTO.email())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Invalid Email");
         }
-        if(optionalUserModel.isPresent()){
+        if (optionalUserModel.isPresent()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("This email already is registered");
         }
         var userModel = new UserModel();
@@ -33,23 +33,35 @@ public class UserService {
         return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(userModel));
     }
 
-    public ResponseEntity<List<UserModel>> getAllUsers(){
+    public ResponseEntity<List<UserModel>> getAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
     }
 
-    public ResponseEntity<Optional<UserModel>> getByEmail(String email){
-        Optional<UserModel> userModel = userRepository.findByemail(email);
-        if(userModel.isPresent()){
+    public ResponseEntity<Optional<UserModel>> getByEmail(String email) {
+        Optional<UserModel> userModel = userRepository.findByEmail(email);
+        if (userModel.isPresent()) {
             return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userModel);
     }
 
-    public ResponseEntity<Optional<UserModel>> getById(UUID id){
+    public ResponseEntity<Optional<UserModel>> getById(UUID id) {
         Optional<UserModel> userModel = userRepository.findById(id);
-        if(userModel.isPresent()){
+        if (userModel.isPresent()) {
             return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userModel);
     }
+
+//    public ResponseEntity<UserModel> changePassword(UUID id, String newPassword) {
+//        Optional<UserModel> optionalUserModel = userRepository.findById(id);
+//        if (optionalUserModel.isEmpty()) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//        }
+//        UserModel userToUpdate = optionalUserModel.get();
+//        userToUpdate.
+//        UserModel updatedUser = userRepository.save(userToUpdate);
+//
+//        return ResponseEntity.ok(updatedUser);
+//    }
 }
